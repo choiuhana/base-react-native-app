d3b135388ae14d1c9569a072d2e2d6f2435d17c7
최창원 <choiuhana@gmail.com>
2023-05-02 08:58:57 +0900

    detail 화면 추가 / choiuhana 라이브러리 추가

--- src/script/ui/Detail/ScreenDetail.tsx
+++ src/script/ui/Detail/ScreenDetail.tsx
@@ -4,55 +4,42 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useTheme } from "@react-navigation/native";
 import { useAppDispatch } from "../../../common/hooks";
 import { resetAll } from "../../redux/slice/accountSlice";
-import { NAVIGATOR_TAB_BOTTOM, Params, SCREEN_DETAIL, SCREEN_MAIN } from "../../navigation/screens";
+import { NAVIGATOR_TAB_BOTTOM, Params, SCREEN_DETAIL } from "../../navigation/screens";
 import Container from "../_common/Container";
 import DefaultText from "../_common/Default/DefaultText";
 import DefaultButton from "../_common/Default/DefaultButton";
-import { sleep, useBroadcastContext } from "@choiuhana/library-react-base";
+import { useBroadcastContext } from "@choiuhana/library-react-base";
 
-type Props = NativeStackScreenProps<Params[typeof NAVIGATOR_TAB_BOTTOM], typeof SCREEN_MAIN>;
+type Props = NativeStackScreenProps<Params[typeof NAVIGATOR_TAB_BOTTOM], typeof SCREEN_DETAIL>;
 
-const ScreenMain = ({ route, navigation }: Props) => {
+const ScreenDetail = ({ route, navigation }: Props) => {
     const dispatch = useAppDispatch();
     const { colors } = useTheme();
-    const { unlisten, listen } = useBroadcastContext();
-
-    const [number, setNumber] = useState(0);
-
-    useEffect(() => {
-        const l = listen("test", (affectedKey, value) => {
-            console.log("??@");
-            setNumber(2);
-        });
-        return () => unlisten(l);
-    }, []);
-
-    console.log(sleep);
+    const { send } = useBroadcastContext();
 
     return (
         <Container style={styles.container}>
-            <DefaultText style={styles.greeting}>Main Screen</DefaultText>
-            <DefaultText style={styles.greeting}>{number}</DefaultText>
+            <DefaultText style={styles.greeting}>Detail Screen</DefaultText>
 
             <DefaultButton
-                text={"Go Detail"}
+                text={"send"}
                 textStyle={{
                     color: colors.on_primary,
                 }}
                 style={{ backgroundColor: colors.primary }}
                 onPress={() => {
-                    navigation.navigate(SCREEN_DETAIL);
+                    send("test");
                 }}
             />
 
             <DefaultButton
-                text={"Logout"}
+                text={"Back"}
                 textStyle={{
                     color: colors.on_primary,
                 }}
                 style={{ backgroundColor: colors.primary }}
                 onPress={() => {
-                    dispatch(resetAll());
+                    navigation.goBack();
                 }}
             />
         </Container>
@@ -72,4 +59,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default ScreenMain;
+export default ScreenDetail;
